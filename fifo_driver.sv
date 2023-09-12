9930cd95de176b74ca885f810658582d0b77c6c9
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-09-12 12:42:21 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -21,7 +21,7 @@ class fifo_driver extends uvm_driver#(fifo_seq_item);
       seq_item_port.get_next_item(req);
       if(req.i_wren == 1)
         main_write(req.data_in);
-      else if(req.i_rden == 1)
+       if(req.i_rden == 1)
         main_read();
       seq_item_port.item_done();
     end
