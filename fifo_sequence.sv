2625bc540b8f61005c50691954f9ccd40d8f7029
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-09-12 11:43:11 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -1,6 +1,5 @@
-
 `include "uvm_macros.svh"
-import uvm_pkg::*;
+import uvm_pkg::*;*
 class fifo_sequence extends uvm_sequence;
  
   `uvm_object_utils(fifo_sequence)
@@ -10,13 +9,35 @@ class fifo_sequence extends uvm_sequence;
     super.new(name);
 
   endfunction
-  task body();
-    fifo=fifo_sequence::type_id::create("fifo_sequence");
-    repeat(5)
-    begin
-      start_item(fifo);
-      fifo.randomize();
-      finish_item(fifo);
+   
+  virtual task body();
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Write REQs ********"), UVM_LOW)
+    repeat(1024) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_wren == 1 && i_rden==0;});
+      finish_item(req);
+    end
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
+    repeat(1024) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_rden == 1 && i_wren==0;});
+      finish_item(req);
+    end
+    `uvm_info(get_type_name(), $sformatf("******** Generate 20 Random REQs ********"), UVM_LOW)
+    repeat(20) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize());
+      finish_item(req);
+    end
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read,Write REQs ********"), UVM_LOW)
+    repeat(1024) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_rden == 1 && i_wren==1;});
+      finish_item(req);
     end
   endtask
     endclass
