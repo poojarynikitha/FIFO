1b16b1d5df4bbe6232b41d715c93aed2b1fb2103
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-09-12 12:46:18 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -20,14 +20,14 @@ class fifo_driver extends uvm_driver#(fifo_seq_item);
     forever begin
       seq_item_port.get_next_item(req);
       if(req.i_wren == 1)
-        main_write(req.data_in);
+        main_write(req.i_wrdata);
        if(req.i_rden == 1)
         main_read();
       seq_item_port.item_done();
     end
   endtask
   
-    virtual task main_write(input [127:0] i_wrdata);
+    virtual task main_write(input [7:0] din);
     @(posedge vif.d_mp.clk)
     vif.d_mp.d_cb.i_wren <= 'b1;
     vif.d_mp.d_cb.i_wrdata <= din;
